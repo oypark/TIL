1fcf69e1ff4f0d352328dda84ea9b0473e1ddd08
oypark <oypark668@gmail.com>
2022-02-22 16:37:37 +0900

    update boj_2562

--- Baekjoon_Online_Judge/BOJ_2562.py
+++ Baekjoon_Online_Judge/BOJ_2562.py
@@ -1,3 +1,4 @@
-numbers = [int(input()) for i in range(9)]
-print(max(numbers))
-print(numbers.index(max(numbers))+1)
+numbers = [int(input()) for _ in range(9)]    # for문에서 쓰지 않는 변수는 i대신 '_'로 표시하는 것이 관행
+max_number = max(numbers)   # 변수를 또 사용할 때는 새로운 변수로 할당하는 것이 효과적
+print(max_number)
+print(numbers.index(max_number) + 1)
