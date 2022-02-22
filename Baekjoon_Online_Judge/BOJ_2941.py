1d7a1ff1458db96428f614287ab27d5def240c8f
oypark <oypark668@gmail.com>
2022-02-22 10:13:33 +0900

    update boj_2941

--- Baekjoon_Online_Judge/BOJ_2941.py
+++ Baekjoon_Online_Judge/BOJ_2941.py
@@ -2,11 +2,12 @@ word = input()
 croa_alpha = ["c=","c-","dz=","d-","lj","nj","s=","z="]
 alpha_cnt = 0
 
+# 크로아티아 알파벳만 카운팅
 for i in croa_alpha:
     cnt = word.count(i)
     alpha_cnt += cnt
     word = word.replace(i," ")
 
 word = word.replace(" ","")
-alpha_cnt += len(word)
+alpha_cnt += len(word)   # 1자리수 알파벳 마저 카운팅
 print(alpha_cnt)
