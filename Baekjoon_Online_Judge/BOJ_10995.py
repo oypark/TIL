a94e38998c58866cfaa0d6bea586671d19e066e1
oypark <oypark668@gmail.com>
2022-02-22 09:30:57 +0900

    update boj_10995

--- Baekjoon_Online_Judge/BOJ_10995.py
+++ Baekjoon_Online_Judge/BOJ_10995.py
@@ -7,12 +7,12 @@ for i in range(n):
     line = line[::-1]
     
 
-## for문 이용
-for i in range(n):
-    # 짝수 번째 줄이면?
-    if i % 2 == 0:
-        print("* " * n)
+# ## for문 이용
+# for i in range(n):
+#     # 짝수 번째 줄이면?
+#     if i % 2 == 0:
+#         print("* " * n)
     
-    # 홀수 번째 줄이면?
-    else:
-        print(" *" * n)
+#     # 홀수 번째 줄이면?
+#     else:
+#         print(" *" * n)
