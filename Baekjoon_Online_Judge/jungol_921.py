8039048120978555618070ff92dd6b15b7043afb
oypark <oypark668@gmail.com>
2022-02-23 11:59:54 +0900

    Update jungol_921

--- Baekjoon_Online_Judge/jungol_921.py
+++ Baekjoon_Online_Judge/jungol_921.py
@@ -1,3 +1,15 @@
+## 기존 풀이
+
+# n = int(input())
+# list = [(i)*(i) for i in range(1,n+1)]
+# print(list)
+
+
+## 피드백
+# 1. 변수명
+# 2. pep8 고려!
+
+
+## 새로운 풀이
 n = int(input())
-list = [(i)*(i) for i in range(1,n+1)]
-print(list)
+print([i*i for i in range(1,n+1)])
