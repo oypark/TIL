6b691d5ed358237e29fcabf89333d6b505787fe9
oypark <oypark668@gmail.com>
2022-02-23 13:03:08 +0900

    Update jungol_929

--- Baekjoon_Online_Judge/jungol_929.py
+++ Baekjoon_Online_Judge/jungol_929.py
@@ -1,3 +1,13 @@
+## 기존 풀이
 n = int(input())
 list = ["No."+str(i+1) for i in range(n)]
 print(list)
+
+## 피드백
+# 1. f-string 이용해서도 고려해보기
+# 2. list 변수명 바꾸기
+
+## 새로운 풀이
+n = int(input())
+numbers = [f"No.{i+1}" for i in range(n)]
+print(numbers)
