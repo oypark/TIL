fd21a85bc3c90976a331b237f1c06fe0d53565fc
oypark <oypark668@gmail.com>
2022-02-23 11:44:51 +0900

    Update boj_11720

--- Baekjoon_Online_Judge/BOJ_11720.py
+++ Baekjoon_Online_Judge/BOJ_11720.py
@@ -1,8 +1,18 @@
+## 최초풀이
+# n = int(input())
+# number = input()
+# list = []
+
+# for i in range(n):
+#     list.append(int(number[i]))
+
+# print(sum(list))
+
+
+## 피드백
+# 1. list는 파이썬 내장함 수 이름이므로 다른 이름으로 바꿔보기
+# 2. 애초에 input을 받을 때 map을 통해서 받아보기
+
 n = int(input())
-number = input()
-list = []
-
-for i in range(n):
-    list.append(int(number[i]))
-
-print(sum(list))
+numbers = list(map(int, input()))
+print(sum(numbers))
