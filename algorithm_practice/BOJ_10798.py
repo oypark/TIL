b7757ce2eeb138d4f75b3633d20ec75842f8e973
oypark <oypark668@gmail.com>
2022-02-25 01:07:06 +0900

    Update annotation boj_10798.py

--- algorithm_practice/BOJ_10798.py
+++ algorithm_practice/BOJ_10798.py
@@ -4,15 +4,19 @@ letters = [input() for _ in range(5)]
 max_len = max(len(letter) for letter in letters)
 pattern = ""
 
+# max_len 보다 짧은 문자열의 빈칸을 .으로 채워주기
 for i, letter in enumerate(letters):
     letter_len = len(letter)
     if letter_len != max_len:
         letters[i] = letter + "." * (max_len - letter_len)
 
+# letters를 세로로 읽어서 문자열로 pattern에 저장
 for vertical in zip(*letters):
     pattern += "".join(vertical)
 
+# 채웠던 . 삭제 후 반환
 print(pattern.replace(".",""))
+
 
 
 ## 다른 풀이
