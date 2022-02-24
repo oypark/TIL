5561ece65a033d6b8f8a6d36995dcee29bf40e2e
oypark <oypark668@gmail.com>
2022-02-25 01:27:33 +0900

    solve boj_2563

---
+++ algorithm_practice/BOJ_2563.py
@@ -0,0 +1,12 @@
+## 색종이
+
+n = int(input())
+paper = [[0] * 100 for _ in range(100)]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1
+
+print(sum(sum(line) for line in paper))
